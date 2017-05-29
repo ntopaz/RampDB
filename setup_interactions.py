fd41734b18ff05b920bdf74d88b6ab3eb7e46a26
nadav <nadavtopaz1@gmail.com>
2017-05-29 07:34:18 -0700

    further ligand loading

--- setup_interactions.py
+++ setup_interactions.py
@@ -5,30 +5,19 @@ from core.views_load_interactions import load_interactions
 def load_int():
 	interaction = {}
 	list = []
-	attributes = {'name':'Amylin receptor 1','name_short': 'AMY1', 'rampfamily':'receptor activity modifying protein 1','gpcrfamily':'calcitonin receptor','ligand':'amylin', 'function':'High affinity for amylin and CGRP'}
-	interaction['Amylin receptor 1'] = attributes
-	attributes = {'name':'Amylin receptor 2','name_short': 'AMY2','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'calcitonin receptor','ligand':'amylin','function':'High affinity for amylin'}
-	interaction['Amylin receptor 2'] = attributes
-	attributes = {'name':'Amylin receptor 3','name_short': 'AMY3','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'calcitonin receptor','ligand':'amylin','function':'High affinity for amylin and low affinity for CGRP'}
-	interaction['Amylin receptor 3'] = attributes
-	attributes = {'name':'Calcitonin gene-related peptide receptor','name_short': 'CGRP','rampfamily':'receptor activity modifying protein 1','gpcrfamily':'calcitonin receptor-like receptor','ligand':'calcitonin gene-related peptide','function':'High affinity for CGRP'}
-	interaction['Calcitonin gene-related peptide receptor'] = attributes
-	attributes = {'name':'Adrenomedullin receptor','name_short': 'AM1','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'calcitonin receptor-like receptor','ligand':'adrenomedullin','function':'100-fold selectivity for Adrenomedullin over CGRP'}
-	interaction['Adrenomedullin receptor'] = attributes
-	attributes = {'name':'Intermedin receptor','name_short': 'AM2','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'calcitonin receptor-like receptor','ligand':'intermedin','function':'Affinity for intermedin and adrenomedullin'}
-	interaction['Intermedin receptor'] = attributes
-	attributes = {'name':'Vasoactive Intestinal Polypeptide Receptor','name_short': 'VIP1_1','rampfamily':'receptor activity modifying protein 1','gpcrfamily':'vasoactive intestinal polypeptide receptor 1','ligand':'vasoactive intestinal polypeptide','function':'Translocation to the cell surface'}
-	interaction['Vasoactive Intestinal Polypeptide Receptor 1_1'] = attributes
-	attributes = {'name':'Vasoactive Intestinal Polypeptide Receptor','name_short': 'VIP1_2','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'vasoactive intestinal polypeptide receptor 1','ligand':'vasoactive intestinal polypeptide','function':'Increased affinity for VIP1 agonists'}
-	interaction['Vasoactive Intestinal Polypeptide Receptor 1_2'] = attributes
-	attributes = {'name':'Vasoactive Intestinal Polypeptide Receptor','name_short': 'VIP1_3','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'vasoactive intestinal polypeptide receptor 1','ligand':'vasoactive intestinal polypeptide','function':'Not enough data to determine function'}
-	interaction['Vasoactive Intestinal Polypeptide Receptor 1_3'] = attributes
-	attributes = {'name':'Parathyroid Hormone Receptor 1','name_short': 'PTHR1','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'parathyroid hormone receptor 1','ligand':'parathyroid hormone','function':'Not enough data to determine function'}
-	interaction['Parathyroid Hormone Receptor 1_2'] = attributes
-	attributes = {'name':'Parathyroid Hormone Receptor 2','name_short': 'PTHR2','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'parathyroid hormone receptor 2','ligand':'parathyroid hormone','function':'Not enough data to determine function'}
-	interaction['Parathyroid Hormone Receptor 2_3'] = attributes
-	attributes = {'name':'Glucagon Receptor','name_short': 'Glucagon','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'glucagon receptor','ligand':'glucagon','function':'Increases glucagon ligand potency and efficacy'}
-	interaction['Glucagon Receptor 1_2'] = attributes
+	interaction['Amylin receptor 1'] = {'name':'Amylin receptor 1','name_short': 'AMY1', 'rampfamily':'receptor activity modifying protein 1','gpcrfamily':'calcitonin receptor','ligand':'amylin', 'function':'High affinity for amylin and CGRP'}
+	interaction['Amylin receptor 2'] = {'name':'Amylin receptor 2','name_short': 'AMY2','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'calcitonin receptor','ligand':'amylin','function':'High affinity for amylin'}
+	interaction['Amylin receptor 3'] = {'name':'Amylin receptor 3','name_short': 'AMY3','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'calcitonin receptor','ligand':'amylin','function':'High affinity for amylin and low affinity for CGRP'}
+	interaction['Calcitonin gene-related peptide receptor'] = {'name':'Calcitonin gene-related peptide receptor','name_short': 'CGRP','rampfamily':'receptor activity modifying protein 1','gpcrfamily':'calcitonin receptor-like receptor','ligand':'calcitonin gene-related peptide alpha','function':'High affinity for CGRP'}
+	interaction['Adrenomedullin receptor'] = {'name':'Adrenomedullin receptor','name_short': 'AM1','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'calcitonin receptor-like receptor','ligand':'adrenomedullin','function':'100-fold selectivity for Adrenomedullin over CGRP'}
+	interaction['Intermedin receptor'] = {'name':'Intermedin receptor','name_short': 'AM2','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'calcitonin receptor-like receptor','ligand':'adrenomedullin 2','function':'Affinity for intermedin and adrenomedullin'} 
+	interaction['Vasoactive Intestinal Polypeptide Receptor 1_1'] = {'name':'Vasoactive Intestinal Polypeptide Receptor','name_short': 'VIP1_1','rampfamily':'receptor activity modifying protein 1','gpcrfamily':'vasoactive intestinal polypeptide receptor 1','ligand':'vasoactive intestinal polypeptide','function':'Translocation to the cell surface'}
+	interaction['Vasoactive Intestinal Polypeptide Receptor 1_2'] = {'name':'Vasoactive Intestinal Polypeptide Receptor','name_short': 'VIP1_2','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'vasoactive intestinal polypeptide receptor 1','ligand':'vasoactive intestinal polypeptide','function':'Increased affinity for VIP1 agonists'}
+	interaction['Vasoactive Intestinal Polypeptide Receptor 1_3'] = {'name':'Vasoactive Intestinal Polypeptide Receptor','name_short': 'VIP1_3','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'vasoactive intestinal polypeptide receptor 1','ligand':'vasoactive intestinal polypeptide','function':'Not enough data to determine function'}
+	interaction['Parathyroid Hormone Receptor 1_2'] = {'name':'Parathyroid Hormone Receptor 1','name_short': 'PTHR1','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'parathyroid hormone receptor 1','ligand':'parathyroid hormone','function':'Not enough data to determine function'}
+	interaction['Parathyroid Hormone Receptor 2_3'] = {'name':'Parathyroid Hormone Receptor 2','name_short': 'PTHR2','rampfamily':'receptor activity modifying protein 3','gpcrfamily':'parathyroid hormone receptor 2','ligand':'parathyroid hormone','function':'Not enough data to determine function'}
+	interaction['Glucagon Receptor 1_2'] = {'name':'Glucagon Receptor','name_short': 'Glucagon','rampfamily':'receptor activity modifying protein 2','gpcrfamily':'glucagon receptor','ligand':'glucagon','function':'Increases glucagon ligand potency and efficacy'}
+
 	load_interactions(json.dumps(interaction))
 
 def main():
--- setup_ligands.py
+++ setup_ligands.py
@@ -8,7 +8,7 @@ def load_lig():
 	list = ["amylin","adrenomedullin","intermedin","calcitonin gene-related peptide","vasoactive intestinal polypeptide","parathyroid hormone","tuberoinfundibular peptide"]
 	ligand["amylin"] = {"name_short":"AMY","chem_id":"16132430","inchi_key":"PLOPBXQQPZYQFA-AXPWDRQUSA-N","sequence":"KCNTATCATQRLANFLVHSSNNFGAILSSTNVGSNTY","type":"Agonist"}
 	ligand["adrenomedullin"] = {"name_short":"AM1","chem_id":"56841671","inchi_key":"ULCUCJFASIJEOE-NPECTJMMSA-N","sequence":"YRQSMNNFQGLRSFGCRFGTCTVQKLAHQIYQFTDKDKDNVAPRSKISPQGY","type":"Agonist"}
-	ligand["calcitonin gene-related peptide alpha"] = {"name_short":"α-CGRP","chem_id":"44563440","inchi_key":"JMJJWZFCOWFIBU-XJVRLEFXSA-N","sequence":"ACDTATCVTHRLAGLLSRSGGVVKNNFVPTNVGSKAF","type":"Agonist"}
+	ligand["calcitonin gene-related peptide alpha"] = {"name_short":"alpha-CGRP","chem_id":"44563440","inchi_key":"JMJJWZFCOWFIBU-XJVRLEFXSA-N","sequence":"ACDTATCVTHRLAGLLSRSGGVVKNNFVPTNVGSKAF","type":"Agonist"}
 	ligand["vasoactive intestinal polypeptide"] = {"name_short":"VIP","chem_id":"16129679","inchi_key":"VBUWHHLIZKOSMS-UHFFFAOYSA-N","sequence":"HSDAVFTDNYTRLRKQMAVKKYLNSILN","type":"Agonist"}
 	ligand["parathyroid hormone"] = {"name_short":"PTH","chem_id":"16129682","inchi_key":"OGBMKVWORPGQRR-UHFFFAOYSA-N","sequence":"SVSEXQLMHNLGKHLNSMERVEWLRKKLQDVHNF","type":"Agonist"}
 	ligand["glucagon"] = {"name_short":"Glucagon","chem_id":"16132283","inchi_key":"MASNOZXLGMXCHN-ZLPAWPGGSA-N","sequence":"HSQGTFTSDYSKYLDSRRAQDFVQWLMNT","type":"Agonist"}
@@ -16,7 +16,7 @@ def load_lig():
 	### new ###
 	ligand["calcitonin"] = {"name_short":"CT","chem_id":"16132288","inchi_key":"MKPOFZGVVSUXKS-JKQNMTHDSA-N","sequence":"CGNLSTCMLGTYTQDFNKFHTFPQTAIGVGAP","type":"Agonist"}
 	ligand["oxyntomodulin"] = {"name_short":"Oxyntomodulin","chem_id":"121494088","inchi_key":"DDYAPMZTJAYBOF-ZMYDTDHYSA-N","sequence":"HSQGTFTSDYSKYLDSRRAQDFVQWLMNTKRNRNNIA","type":"Agonist"}
-	ligand["calcitonin gene-related peptide beta"] = {"name_short":"β-CGRP","chem_id":"N/A","inchi_key":"N/A","sequence":"ACNTATCVTHRLAGLLSRSGGMVKSNFVPTNVGSKAF","type":"Agonist"}
+	ligand["calcitonin gene-related peptide beta"] = {"name_short":"beta-CGRP","chem_id":"N/A","inchi_key":"N/A","sequence":"ACNTATCVTHRLAGLLSRSGGMVKSNFVPTNVGSKAF","type":"Agonist"}
 	ligand["pramlintide"] = {"name_short":"pramlintide","chem_id":"70691388","inchi_key":"TZIRZGBAFTZREM-MKAGXXMWSA-N","sequence":"KCNTATCATQRLANFLVHSSNNFGPILPPTNVGSNTY","type":"Agonist"}
 	ligand["AC187"] = {"name_short":"AC187","chem_id":"16133792","inchi_key":"ZLFXHYNEZYAYPG-AABHONRUSA-N","sequence":"VLGKLSQELHKLQTYPRTNTGSNTY","type":"Antagonist"}
 	ligand["adrenomedullin 2"] = {"name_short":"AM2","chem_id":"N/A","inchi_key":"N/A","sequence":"TQAQLLRVGCVLGTCQVQNLSHRLWQLMGPAGRQDSAPVDPSSPHSY","type":"Agonist"}
