1b7cb86affec26fd7134954a397f11fbefa35ee0
nadav <nadavtopaz1@gmail.com>
2017-06-29 15:07:47 -0700

    additional parameter

--- core/views_load_proteins.py
+++ core/views_load_proteins.py
@@ -1,4 +1,5 @@
 import sys,os,json
+import pprint as pp
 #from django.conf import settings
 #settings.configure()
 os.environ["DJANGO_SETTINGS_MODULE"] = "rampdb.settings"
@@ -10,25 +11,24 @@ from models import *
 # Create your views here.
 
 @transaction.atomic
-def load_db(data):
-	short_name = {'receptor activity modifying protein 1':'Ramp 1','receptor activity modifying protein 2':'Ramp 2','receptor activity modifying protein 3':'Ramp 3',
-                        'receptor activity modifying protein 4':'Ramp 4','receptor activity modifying protein 5':'Ramp 5','calcitonin receptor':'CT','calcitonin receptor-like receptor':'CLR',
-                        'vasoactive intestinal polypeptide receptor 1':'VIP 1','parathyroid hormone receptor 1':'PTHR1',
-			'parathyroid hormone receptor 2':'PTHR2','glucagon receptor':'glucagon'}
-
+def load_db(data,family_data):
 	source_obj, source_created = Source.objects.get_or_create(name="NCBI",url="www.ncbi.nlm.nih.gov")
-	for family in sorted(data.keys()):
-		family_obj, family_created = Family.objects.get_or_create(name = family, name_short=short_name[family])
-		for protein in sorted(data[family].keys()):
-			organism_obj, organism_created = Organism.objects.update_or_create(name=data[family][protein]['org'])
-			protein_obj, protein_created = Protein.objects.update_or_create(
-							name = data[family][protein]['desc'],
-							sequence = data[family][protein]['seq'],
-							reference_id = protein,
-							family = family_obj,
-							organism = organism_obj,
-							source = source_obj,
-							)
+	for family in family_data:
+		family_name = family_data[family]["name"]
+		pdb_id = family_data[family]["pdb_id"]
+		gtp_id = family_data[family]["gtp_id"]
+		family_obj, family_created = Family.objects.get_or_create(name= family_name, name_short=family, pdb_id = pdb_id, gtp_id = gtp_id)
+		if family_name in data:
+			for protein in sorted(data[family_name].keys()):
+				organism_obj, organism_created = Organism.objects.update_or_create(name=data[family_name][protein]['org'])
+				protein_obj, protein_created = Protein.objects.update_or_create(
+								name = data[family_name][protein]['desc'],
+								sequence = data[family_name][protein]['seq'],
+								reference_id = protein,
+								family = family_obj,
+								organism = organism_obj,
+								source = source_obj,
+								)
 
 
 
